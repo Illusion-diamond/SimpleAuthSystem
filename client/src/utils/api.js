f6ad84bccf04e556ebd5d69b3f428017a9e8bb1c
Illusion-diamond <100913532+Illusion-diamond@users.noreply.github.com>
2026-08-04 09:06:49 +0530

    Update API base URL to use environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://simpleauthsystem.onrender.com",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 API.interceptors.request.use((req) => {
