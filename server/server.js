b6c740c7aa470ddc67affc00a9d05aa36638167b
Illusion-diamond <100913532+Illusion-diamond@users.noreply.github.com>
2026-08-04 09:15:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
 cors({
-origin: 'http://localhost:5173',
+origin: "https://simpleauthsystem-1.onrender.com",
 credentials: true,
 methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 })
